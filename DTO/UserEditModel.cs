e241ac40775861d760760733946fa5bd1a16a94d
ISLAM EMON <emon78697@gmail.com>
2025-01-24 05:01:40 +0700

    added UserEditModel class file and implemeted code, updated user service for update profie feature

---
+++ DTO/UserEditModel.cs
@@ -0,0 +1,14 @@
+﻿using DTO.Enums;
+using System;
+
+namespace DTO
+{
+    public class UserEditModel
+    {
+        public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
