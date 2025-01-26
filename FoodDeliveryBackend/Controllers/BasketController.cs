f50d0323002951c0f6b6076b72798380560d1b09
ISLAM EMON <emon78697@gmail.com>
2025-01-26 20:02:48 +0700

    Added BasketControoler and implemented code got Add dish to cart

---
+++ FoodDeliveryBackend/Controllers/BasketController.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [HttpPost("dish/{dishId}")]
+        public async Task<IActionResult> AddDishToBasket(Guid dishId)
+        {
+            var userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            await _basketService.AddDishToBasketAsync(userId, dishId);
+            return Ok();
+        }
+
+       
+    }
+}
