66b95f67a21bb9bbfc1e45aabb5105196dbcca64
ISLAM EMON <emon78697@gmail.com>
2025-01-26 08:41:04 +0700

    Added BasketItems.cs, Updated ApplicationDbContext for Basket entity

---
+++ DAL/Models/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DAL.Models;
+
+namespace DAL.Models
+{
+    public class BasketItem
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid DishId { get; set; }
+        public int Amount { get; set; }
+        public Dish Dish { get; set; } // Navigation property
+    }
+}
