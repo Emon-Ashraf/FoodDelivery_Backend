a2f5ec6822176b2bf5e469875124292b7f62cc28
ISLAM EMON <emon78697@gmail.com>
2025-01-26 08:43:41 +0700

    Updated ApplicationDbContext for Basket entity

--- DAL/Data/ApplicationDbContext.cs
+++ DAL/Data/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace DAL.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Dish> Dishes { get; set; }
+
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
