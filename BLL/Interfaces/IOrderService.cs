850bf874d140d1444ad7a43830670883068e61d5
ISLAM EMON <emon78697@gmail.com>
2025-01-27 07:09:31 +0700

    Added OrderInfoDto.cs and Implemented, Updated IOrderService.cs to add all Async

--- BLL/Interfaces/IOrderService.cs
+++ BLL/Interfaces/IOrderService.cs
@@ -7,6 +7,10 @@ namespace BLL.Interfaces
 {
     public interface IOrderService
     {
+        Task<List<OrderInfoDto>> GetOrdersAsync(Guid userId);
+        Task<OrderDto> GetOrderByIdAsync(Guid userId, Guid orderId);
+        Task<OrderDto> CreateOrderAsync(Guid userId, OrderCreateDto orderCreateDto);
+        Task ConfirmOrderDeliveryAsync(Guid userId, Guid orderId);
 
     }
 }
---
+++ DTO/OrderInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DTO.Enums;
+
+namespace DTO
+{
+    public class OrderInfoDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DeliveryTime { get; set; }
+        public DateTime OrderTime { get; set; }
+        public OrderStatus Status { get; set; }
+        public double Price { get; set; }
+    }
+}
